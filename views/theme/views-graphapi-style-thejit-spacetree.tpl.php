141cbf8283525d12cf226126ddad6a72f7728b05
Clemens Tolboom <clemens@build2be.com>
2011-12-29 17:06:56 +0100

    Add support for thejit ... needs more refactoring.

--- views/theme/views-graphapi-style-thejit-spacetree.tpl.php
+++ views/theme/views-graphapi-style-thejit-spacetree.tpl.php
@@ -6,11 +6,11 @@ if (isset($view->override_path)) {       // inside live preview
 }
 elseif (isset($options['using_views_api_mode']) && $options['using_views_api_mode']) {
   // We're in Views API mode.
-  print "DOT" . $xml;
+  print $xml;
 }
 else {
   // TODO: this could be a json callback
   //drupal_add_http_header("Content-Type", "$content_type; charset=utf-8");
-  print "DOT" . $xml;
+  print $xml;
   //exit;
 }
